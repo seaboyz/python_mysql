504a3ef2d304645f1aca709618403b1426806ea7
seaboyz <seaboyz@hotmail.com>
2022-08-28 14:45:03 -0500

    U

---
+++ setup.py
@@ -0,0 +1,14 @@
+import mysql.connector
+from database import cursor
+
+DB_NAME = 'test'
+
+
+def create_db():
+    cursor.execute(
+        f"CREATE DATABASE IF NOT EXISTS {DB_NAME} DEFAULT CHARACTER SET 'utf8'"
+    )
+    print(f"Database {DB_NAME} created")
+
+
+
