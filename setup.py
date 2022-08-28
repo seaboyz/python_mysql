f95a06f8fe7f1f26891b3a7df18b990a47386173
seaboyz <seaboyz@hotmail.com>
2022-08-28 15:18:13 -0500

    Create tables

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
+from mysql.connector import errorcode
 import mysql.connector
 from database import cursor
-
 DB_NAME = 'test'
+
+TABLES = {}
+
+TABLES['logs'] = (
+    "CREATE TABLE `logs` ("
+    " `id` int(11) NOT NULL AUTO_INCREMENT,"
+    " `text` varchar(250) NOT NULL,"
+    " `user` varchar(250) NOT NULL,"
+    " `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+    " PRIMARY KEY (`id`)"
+    ") ENGINE=InnoDB"
+)
 
 
 def create_db():
@@ -11,4 +23,20 @@ def create_db():
     print(f"Database {DB_NAME} created")
 
 
+def create_tables():
+    cursor.execute(f"USE {DB_NAME}")
+    for name, ddl in TABLES.items():
+        try:
+            print(f"Creating table {name}")
+            cursor.execute(ddl)
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                print(f"Table {name} already exists")
+            else:
+                print(err.msg)
+        else:
+            print(f"Table {name} created")
+
+
 create_db()
+create_tables()
